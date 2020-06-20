af0acc9168daef460805ec03a6c2686f14ed13ba
Rezyapkin <rezyapkin@yandex.ru>
2020-06-20 10:20:02 +0300

    complete task5

---
+++ lesson_2/task_5.js
@@ -0,0 +1,20 @@
+function sum(a, b) {
+    return a + b;
+}
+
+function sub(a, b) {
+    return a - b;
+}
+
+function mul(a, b) {
+    return a * b;
+}
+
+function dev(a, b) {
+    return a * b;
+}
+
+alert(sum(5, 4));
+alert(mul(3, 2));
+alert(dev(4, 2));
+alert(sub(8, 2));
